79264302fc9474a5adff8ed0d0e497095d1df971
Tony-Tor <Tony.Tor2016@yandex.ru>
2021-11-22 15:06:19 +0400

    Initial Commit

---
+++ src/main/java/com/tonytor/myportfolio/site/myportfoliosite/MyportfoliositeApplication.java
@@ -0,0 +1,13 @@
+package com.tonytor.myportfolio.site.myportfoliosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyportfoliositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyportfoliositeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonytor/myportfolio/site/myportfoliosite/MyportfoliositeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonytor.myportfolio.site.myportfoliosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyportfoliositeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
